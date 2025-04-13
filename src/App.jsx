2b23fce83d66c77a5b8176ed138eded199dc7aa3
Tejaswini-231 <tejaswini.reddy231@gmail.com>
2025-04-13 18:48:27 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import {MyComponent,Conditions,PersonComponent} from './practice '
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <MyComponent/>
+        <Conditions/>
+        <PersonComponent/>
+      </div>
+      <h1>Day-1 of React Js</h1>
+      <div className="card">
+        <button onClick={()=>{console.log("hello!!")}}>Button</button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
